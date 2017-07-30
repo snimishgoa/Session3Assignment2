fb8583b39804219749867e75e5f47cf8eaa9c94b
unknown <SHIKERKAR@nbshikerkar.IAZI.LOCAL>
2017-07-30 21:44:02 +0200

    Initial commit

---
+++ car.js
@@ -0,0 +1,22 @@
+class Car {
+    get name() {
+        return this._name;
+    }
+    constructor(name) {
+        this._name = name;
+    }
+    start() {
+        console.log(`Car ${this.name} started!`);
+    }
+    drive() {
+        console.log(`Driving car: ${this.name}`);
+    }
+    getPosition() {
+        return { latitude: 46.23, longitude: 8.453 };
+    }
+}
+// Test the code
+let car = new Car("Hundai i30");
+car.start();
+car.drive();
+console.log(JSON.stringify(car.getPosition()));
---
+++ car.ts
@@ -0,0 +1,41 @@
+interface CarPosition{
+    latitude: number;
+    longitude: number;
+}
+
+interface Drivable {
+    start(): void;
+    drive(): void;
+    getPosition(): CarPosition;
+}
+
+class Car implements Drivable{
+    
+    private _name : string;
+    public get name() : string {
+        return this._name;
+    }
+   
+    constructor(name: string){
+        this._name = name;
+    }
+
+    start(): void{
+        console.log(`Car ${this.name} started!`);        
+    }
+
+    drive(): void{
+        console.log(`Driving car: ${this.name}`);
+        
+    }
+
+    getPosition(): CarPosition{
+        return { latitude :46.23, longitude: 8.453 };
+    }
+}
+
+// Test the code
+let car: Car = new Car("Hundai i30");
+car.start();
+car.drive();
+console.log(JSON.stringify(car.getPosition()));
